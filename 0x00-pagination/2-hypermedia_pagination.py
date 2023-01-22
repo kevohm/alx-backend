a8edda2208c7345e560b5ea2ea5ef9a0b032e318
kevohm <kevinkbet@gmail.com>
2023-01-22 10:31:16 +0000

    finished_task-2

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+2-hypermedia_pagination
+"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """find the correct indexes to paginate the dataset
+        correctly and return the appropriate page of the dataset
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        try:
+            start = page_size * (page - 1)
+            end = page_size * page
+            data = self.dataset()
+            return data[start:end]
+        except(IndexError):
+            return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
+        """
+        data = self.get_page(page, page_size)
+        next_data = self.get_page(page + 1, page_size)
+        total = math.ceil(len(self.__dataset) / page_size)
+        next_page = None
+        prev_page = None
+        if len(next_data) > 0:
+            next_page = page + 1
+        if page > 1:
+            prev_page = page - 1
+        return {"page_size": len(data), "page": page, "data": data,
+                "next_page": next_page, "prev_page": prev_page,
+                "total_pages": total}
