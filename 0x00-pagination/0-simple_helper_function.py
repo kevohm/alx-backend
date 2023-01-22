ef95c3d228220f0c40d42fffe28b1debe6292c6e
kevohm <kevinkbet@gmail.com>
2023-01-22 08:49:49 +0000

    finished_task_0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+0-simple_helper_function
+"""
+
+def index_range(page, page_size):
+    """
+    return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
+    start = page_size * (page - 1)
+    end = page_size * page
+    return (start, end)
